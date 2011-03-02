acde4a608cf9cf3f2fcf39b1d7164836e54b54b0
Ben Blackburne <bpb@lysozyme.net>
2011-03-02 14:47:51 +0000

    homDist, but slow
    Due to code on FIXME line?

--- src/Alignment.hs
+++ src/Alignment.hs
@@ -42,3 +42,40 @@ toFasta aln = stringList where --foldl (++) "" stringList where
                  seqList = zip (names aln) (sequences aln)
                  toSeqStr :: (String,String) -> String
                  toSeqStr (name,seq) = ">" ++ name ++ "\n" ++ seq ++ "\n"
+
+numberify :: ListAlignment -> [[Int]]
+numberify aln = map nfy myseqs where
+        myseqs = sequences aln
+        nfy = numberMap 0
+        numberMap i [] = []
+        numberMap i ('-':xs) = -1 : numberMap i xs
+        numberMap i (x:xs) = i : numberMap (i+1) xs
+
+homDist :: ListAlignment -> ListAlignment -> Double
+homDist aln1 aln2 = answer (diff pairs1 pairs2) where
+        
+        answer :: (Int,Int) -> Double
+        answer (numPairs,numDiffs) = (fromIntegral numDiffs) / (fromIntegral numPairs)
+        
+        pairs :: [[Int]] -> [[(Int,Int)]]
+        pairs [] = []
+        pairs (x:y:xs) = zip x y : zip y x :  pairs (x:xs)  ++ pairs (y:xs) -- FIXME 
+        pairs (x:[]) = []
+
+        pairs1 = pairs (numberify aln1)
+        pairs2 = pairs (numberify aln2)
+
+        addT (a,b) (c,d) = (a+c,b+d)
+
+        diff2 :: [(Int,Int)] -> [(Int,Int)] -> (Int,Int)
+        diff2 ((-1,x2):xs) ((-1,y2):ys) = (diff2 xs ys) 
+        diff2 ((-1,x2):xs) y = addT (diff2 xs y) (0,0)
+        diff2 x ((-1,y2):ys) = addT (diff2 x ys) (0,0)
+        diff2 ((x1,x2):xs) ((y1,y2):ys) | x1 == y1 && x2==y2 = addT (diff2 xs ys) (1,0)
+        diff2 ((x1,x2):xs) ((y1,y2):ys) | x1 == y1 = addT (diff2 xs ys) (1,1)
+    --    diff2 (x:xs) (y:ys) = diff2 xs ys
+        diff2 [] [] = (0,0)
+
+        diff :: [[(Int,Int)]] -> [[(Int,Int)]] -> (Int,Int)
+        diff [] [] = (0,0)
+        diff (x:xs) (y:ys) = addT (diff2 x y) (diff xs ys)
